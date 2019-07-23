63ef70b1c0d183d0a57e04f3497c7899da220e18
Nikita Rakov <nrakov@yotateam.com>
2019-07-23 16:11:26 +0300

    Initial Commit

---
+++ FlickrTwenty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlickrTwenty
+//
+//  Created by Nikita Rakov on 23/07/2019.
+//  Copyright © 2019 Nikita Rakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
